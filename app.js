1bb94c366d5dd5992b66362b354d902a5af715ee
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-23 19:30:17 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+// let Signup = () => {
+//  let email = document.getElementById("email")
+//  let password = document.getElementById("password")
+
+
+// firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+// .then ((result)=> {
+//     console.log(result)
+
+// })
+// .catch(function(error) {
+
+    
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log(errorMessage)
+//     // ...
+//   });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var promise = new Promise(function (resolve, reject) {
+//     var zinger = "available"
+//     if (zinger === "availabl ") {
+//         resolve("this is a promise");
+//     } else {
+//         reject = "nahi khaya hmm ne zinger"
+//     }
+
+
+
+// })
+
+
+
+
+// let getData = (render_data) => {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(data => {
+//          render_data(data)
+// })
+
+
+// getData(render_data);
+
+
+// let render_data = () => {
+
+// }
+// }
+
